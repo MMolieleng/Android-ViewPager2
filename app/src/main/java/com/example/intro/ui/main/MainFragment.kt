69eaf028b2880bc209d9538868a1d7b49b511c65
Mohale Molieleng <mmolieleng@jhb.dvt.co.za>
2020-02-19 00:14:06 +0200

    added more implementation

--- app/src/main/java/com/example/intro/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/intro/ui/main/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.example.intro.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.intro.R
 
 class MainFragment : Fragment() {
@@ -15,18 +18,56 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var viewPager: ViewPager2
+    private lateinit var onBoardingAdapter: OnBoardingAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        var view = inflater.inflate(R.layout.main_fragment, container, false)
+        onBoardingAdapter = OnBoardingAdapter(this)
+        viewPager = view.findViewById(R.id.view_pager)
+        viewPager.adapter = onBoardingAdapter
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+}
+
+
+class OnBoardingAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int {
+        return 3
     }
 
+    override fun createFragment(position: Int): Fragment {
+        val page = IntroPage()
+        page.arguments = Bundle().apply {
+            putInt("position", position + 1)
+        }
+        return page
+    }
+}
+
+class IntroPage : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_intro_page_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey("position") }?.apply {
+            val textView: TextView = view.findViewById(R.id.page_title)
+            textView.text = getInt("position").toString()
+        }
+    }
 }
