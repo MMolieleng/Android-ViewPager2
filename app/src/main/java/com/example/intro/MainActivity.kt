c1efa0ec0f8f561bb0820024be415b268c28975e
Mohale Molieleng <mmolieleng@jhb.dvt.co.za>
2020-02-18 23:48:43 +0200

    added shareprefs and viewpager lib

--- app/src/main/java/com/example/intro/MainActivity.kt
+++ app/src/main/java/com/example/intro/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.intro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.intro.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var PRIVATE_MODE = 0
+    private val PREF_NAME = "first_time_app_launch"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+
+        val sharedPref = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        if (sharedPref.getBoolean(PREF_NAME, false)) {
+            Toast.makeText(this, "Old User", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "First time login", Toast.LENGTH_LONG).show()
         }
     }
 
